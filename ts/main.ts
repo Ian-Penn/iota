2d50cda829f86f947967ee2d958c3c610b4f9c6f
Ian Penn <186482445+Ian-Penn@users.noreply.github.com>
2024-10-29 12:17:29 -0400

    added REPL.ts

--- ts/main.ts
+++ ts/main.ts
@@ -1,11 +1,10 @@
+import path from "path";
+
 import * as utilities from "./utilities.js";
 import logger from "./logger.js";
-import { CompilerOptions, Module } from "./Module.js";
-import { getIndicatorText, removeDuplicateErrors } from "./report.js";
+import { Module } from "./Module.js";
 import { setUpBuiltins } from "./builtin.js";
-import path from "path";
-import readline from 'readline';
-import { stdout } from "process";
+import { startREPL } from "./REPL.js";
 
 // function readDB(): DB {
 // 	const DBtextPath = path.join(path.dirname(options.filePath), "db.json");
@@ -75,117 +74,6 @@ switch (mode) {
 	default: {
 		utilities.TODO_addError();
 	}
-}
-
-function startREPL(module: Module) {
-	const eraseLine = "\x1B[2K";
-	const cursorBack = "\x1B[D";
-	const prompt = "(*)";
-	
-	let currentLine = "";
-	let currentLineAfterCursor = "";
-	let historyI = 0;
-	let history: string[] = [];
-	
-	// function writeEraseCurrentLine() {
-	// 	stdout.write("\r" + eraseInLine);
-	// }
-	
-	function writeCurrentLine() {
-		stdout.write("\r" + eraseLine + prompt + currentLine + currentLineAfterCursor + cursorBack.repeat(currentLineAfterCursor.length));
-	}
-	
-	function printErrorsAndEvaluations() {
-		module.runEvalQueue();
-		for (let i = 0; i < module.errors.length; i++) {
-			const error = module.errors[i];
-			stdout.write(error.msg + "\n");
-		}
-		for (let i = 0; i < module.topLevelEvaluations.length; i++) {
-			const output = module.topLevelEvaluations[i];
-			stdout.write(output.msg + "\n");
-		}
-		module.errors = [];
-		module.topLevelEvaluations = [];
-	}
-	
-	printErrorsAndEvaluations();
-	readline.emitKeypressEvents(process.stdin);
-	process.stdin.setRawMode(true);
-	writeCurrentLine();
-	
-	process.stdin.on("keypress", (chunk, key) => {
-		// console.log(key);
-		
-		if (key.name == "escape" || key.sequence == "\x04") {
-			stdout.write("\n");
-			process.exit();
-		} else if (key.sequence == "\x03") { // ^C
-			currentLine = "";
-			stdout.write("\n");
-			writeCurrentLine();
-			return;
-		} else if (key.name == "up") {
-			historyI = Math.max(0, historyI-1);
-			let past = history[historyI];
-			if (past != undefined) {
-				currentLine = past;
-			}
-			writeCurrentLine();
-			return;
-		} else if (key.name == "down") {
-			historyI = Math.min(historyI+1, history.length);
-			let past = history[historyI];
-			if (past != undefined) {
-				currentLine = past;
-			}
-			writeCurrentLine();
-			return;
-		} else if (key.name == "left") {
-			if (currentLine == "") {
-				return;
-			}
-			currentLineAfterCursor = currentLine[currentLine.length-1] + currentLineAfterCursor;
-			currentLine = currentLine.slice(0, currentLine.length-1);
-			writeCurrentLine();
-			return;
-		} else if (key.name == "right") {
-			if (currentLineAfterCursor == "") {
-				return;
-			}
-			currentLine = currentLine + currentLineAfterCursor[0];
-			currentLineAfterCursor = currentLineAfterCursor.slice(1);
-			writeCurrentLine();
-			return;
-		}
-		
-		let char: string = key.sequence;
-		if (char == "\r") char = "\n";
-		
-		if (char == "\n") {
-			currentLine += currentLineAfterCursor;
-			currentLineAfterCursor = "";
-			if (currentLine.length != 0) {
-				history.push(currentLine);
-				historyI = history.length;
-			}
-			
-			stdout.write("\n");
-			module.addText("REPL", currentLine);
-			printErrorsAndEvaluations();
-			currentLine = "";
-			writeCurrentLine();
-		} else if (key.name == "backspace") {
-			if (currentLine.length > 0) {
-				currentLine = currentLine.slice(0, currentLine.length-1);
-				// stdout.write("\x08 \x08");
-				writeCurrentLine();
-			}
-		} else {
-			currentLine += char;
-			writeCurrentLine();
-		}
-	});
 }
 
 // logger.printFileAccessLogs();
