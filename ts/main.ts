72f50d520518899dbadeff260eb20378833158b1
Ian Penn <186482445+Ian-Penn@users.noreply.github.com>
2024-10-29 20:58:29 -0400

    added helpText

--- ts/main.ts
+++ ts/main.ts
@@ -6,73 +6,82 @@ import { Module } from "./Module.js";
 import { setUpBuiltins } from "./builtin.js";
 import { startREPL } from "./REPL.js";
 
-// function readDB(): DB {
-// 	const DBtextPath = path.join(path.dirname(options.filePath), "db.json");
-// 	const DBtext = utilities.readFile(DBtextPath);
-// 	console.log("DBtext:", DBtext);
-// 	const db = JSON.parse(DBtext) as DB;
-// 	console.log("db:", db);
-// 	return db;
-// }
+function nextArg(): string {
+	return process.argv[i++];
+}
+
+const helpText = `
+flags:
+  -log ~ enable compiler logging
+
+modes:
+  init <modulePath>
+  runFile <modulePath> <filePath>
+  start <modulePath>
+`;
 
 setUpBuiltins();
 
 let i = 2;
 
-function nextArg(): string {
-	return process.argv[i++];
+if (process.argv[i] == undefined) {
+	console.log(helpText);
+} else {
+	main();
 }
 
-while (process.argv[i].startsWith("-")) {
-	const flag = nextArg();
-	if (flag == "-log") {
-		logger.enableLogs();
-	} else {
-		utilities.TODO_addError();
-	}
-}
-
-const mode = nextArg();
-
-switch (mode) {
-	case "init": {
-		const arg = nextArg();
-		const basePath = path.dirname(arg);
-		const name = path.basename(arg);
-		
-		const module = new Module(basePath, name);
-		module.saveToFileSystem();
-		break;
+function main() {
+	while (process.argv[i].startsWith("-")) {
+		const flag = nextArg();
+		if (flag == "-log") {
+			logger.enableLogs();
+		} else {
+			utilities.TODO_addError();
+		}
 	}
 	
-	case "runFile": {
-		const arg = nextArg();
-		const basePath = path.dirname(arg);
-		const name = path.basename(arg);
-		
-		const filePath = nextArg();
-		
-		const module = new Module(basePath, name);
-		module.addText(filePath, utilities.readFile(filePath));
-		module.runEvalQueue();
-		module.outputErrorsAndEvaluations(true);
-		module.saveToFileSystem();
-		break;
-	}
+	const mode = nextArg();
 	
-	case "start": {
-		const arg = nextArg();
-		const basePath = path.dirname(arg);
-		const name = path.basename(arg);
+	switch (mode) {
+		case "init": {
+			const modulePath = nextArg();
+			const basePath = path.dirname(modulePath);
+			const name = path.basename(modulePath);
+			
+			const module = new Module(basePath, name);
+			module.saveToFileSystem();
+			break;
+		}
 		
-		const module = new Module(basePath, name);
-		module.loadFromFileSystem();
-		startREPL(module);
-		break;
-	}
-
-	default: {
-		utilities.TODO_addError();
+		case "runFile": {
+			const modulePath = nextArg();
+			const basePath = path.dirname(modulePath);
+			const name = path.basename(modulePath);
+			
+			const filePath = nextArg();
+			
+			const module = new Module(basePath, name);
+			module.addText(filePath, utilities.readFile(filePath));
+			module.runEvalQueue();
+			module.outputErrorsAndEvaluations(true);
+			module.saveToFileSystem();
+			break;
+		}
+		
+		case "start": {
+			const modulePath = nextArg();
+			const basePath = path.dirname(modulePath);
+			const name = path.basename(modulePath);
+			
+			const module = new Module(basePath, name);
+			module.loadFromFileSystem();
+			startREPL(module);
+			break;
+		}
+	
+		default: {
+			utilities.TODO_addError();
+		}
 	}
 }
 
